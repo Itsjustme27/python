c762cf402455a6600db260a3e54ee251b51b108a
Itsjustme27 <reachprayush@gmail.com>
2024-04-14 10:01:33 +0545

    binary-searching

---
+++ Searching(array)/binary-searching.py
@@ -0,0 +1,27 @@
+
+def search():
+    arr = [10, 20, 30]
+    lower = 0
+    upper = 2
+    finding = 0
+    item = int(input("Enter a number: "))
+
+    while lower <= upper:
+        mid = (lower + upper) // 2
+
+        if arr[mid] == item:
+            finding = 1
+            break
+
+        if arr[mid] < item:
+            lower = mid + 1
+        else:
+            upper = mid - 1
+
+    if finding == 1:
+        print(f"Item found at {mid}")
+    else:
+        print("Item not found")
+
+
+search()
